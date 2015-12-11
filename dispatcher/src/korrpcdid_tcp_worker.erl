bd843894ca3a1ad1d2c58616b61507bba5a11d61
Stanislaw Klekot <dozzie@jarowit.net>
2015-12-11 18:28:07 +0100

    Added decoding the request in korrpcdid_tcp_worker.

--- dispatcher/src/korrpcdid_tcp_worker.erl
+++ dispatcher/src/korrpcdid_tcp_worker.erl
@@ -1,7 +1,11 @@
 %%%---------------------------------------------------------------------------
 %%% @private
 %%% @doc
-%%%   Client connection worker.
+%%%   Client connection worker initializer.
+%%%   A process running this module reads a single JSON message from its TCP
+%%%   client, parses it as a request and either responds to the request when
+%%%   it's result is immediate, or switches the running module to the
+%%%   appropriate module to carry it out.
 %%% @end
 %%%---------------------------------------------------------------------------
 
@@ -18,9 +22,35 @@
 -export([code_change/3]).
 
 %%%---------------------------------------------------------------------------
+%%% types {{{
+
+-define(TCP_READ_INTERVAL, 100).
 
 -record(state, {socket}).
 
+-type request_call() ::
+  {call,
+    Procedure :: binary(),
+    Arguments :: korrpc_json:jarray() | korrpc_json:jhash(),
+    Host :: binary(),
+    Timeout :: timeout(),
+    MaxExecTime :: timeout()}.
+
+-type request_cancel() :: {cancel, job_id()}.
+
+-type request_get_result() :: {get_result, job_id(), wait | no_wait}.
+
+-type request_follow_stream() ::
+    {follow_stream, job_id(), since, non_neg_integer()}
+  | {follow_stream, job_id(), recent, non_neg_integer()}.
+
+-type request_read_stream() ::
+    {read_stream, job_id(), since, non_neg_integer()}
+  | {read_stream, job_id(), recent, non_neg_integer()}.
+
+-type job_id() :: string().
+
+%%% }}}
 %%%---------------------------------------------------------------------------
 %%% supervision tree API
 %%%---------------------------------------------------------------------------
@@ -48,6 +78,9 @@ start_link(Socket) ->
 %% @doc Initialize event handler.
 
 init([Socket] = _Args) ->
+  % we can't read the request here, as parent supervisor waits init() to
+  % return
+  inet:setopts(Socket, [binary, {packet, line}, {active, false}]),
   State = #state{
     socket = Socket
   },
@@ -82,8 +115,37 @@ handle_cast(_Request, State) ->
 %% @doc Handle incoming messages.
 
 handle_info(timeout = _Message, State = #state{socket = Socket}) ->
-  gen_tcp:send(Socket, "lorem ipsum dolor sit amet\n"),
-  {stop, normal, State};
+  case read_request(Socket, ?TCP_READ_INTERVAL) of
+    {call, _Proc, _Args, _Host, _Timeout, _MaxExecTime} -> % long running
+      put('$worker_function', call),
+      % TODO: gen_server:enter_loop(korrpcdid_tcp_worker_call)
+      {noreply, State, 0};
+    {cancel, _JobID} -> % immediate
+      put('$worker_function', cancel),
+      % TODO: find appropriate call worker, send it cancellation
+      {noreply, State, 0};
+    {get_result, _JobID, wait} -> % long running
+      put('$worker_function', get_result),
+      % TODO: gen_server:enter_loop(korrpcdid_tcp_worker_call)
+      {noreply, State, 0};
+    {get_result, _JobID, no_wait} -> % immediate
+      put('$worker_function', get_result),
+      % TODO: read from korrpc_sdb
+      {noreply, State, 0};
+    {follow_stream, _JobID, _Mode, _ModeArg} -> % long running
+      put('$worker_function', follow_stream),
+      % TODO: gen_server:enter_loop(korrpcdid_tcp_worker_call)
+      {noreply, State, 0};
+    {read_stream, _JobID, _Mode, _ModeArg} -> % immediate
+      % TODO: read from korrpc_sdb
+      put('$worker_function', read_stream),
+      {noreply, State, 0};
+    timeout ->
+      % yield for next system message if any awaits our attention
+      {noreply, State, 0};
+    {error, Reason} ->
+      {stop, Reason, State}
+  end;
 
 %% unknown messages
 handle_info(_Message, State) ->
@@ -101,6 +163,96 @@ code_change(_OldVsn, State, _Extra) ->
 
 %% }}}
 %%----------------------------------------------------------
+
+%%%---------------------------------------------------------------------------
+
+%% @doc Read a request from TCP socket.
+
+-spec read_request(gen_tcp:socket(), timeout()) ->
+    request_call()
+  | request_cancel()
+  | request_get_result()
+  | request_follow_stream()
+  | request_read_stream()
+  | timeout
+  | {error, term()}.
+
+read_request(Socket, Timeout) ->
+  case gen_tcp:recv(Socket, 0, Timeout) of
+    {ok, Line} ->
+      try
+        decode_request(Line)
+      catch
+        error:{badmatch,_}    -> {error, bad_protocol};
+        error:{case_clause,_} -> {error, bad_protocol}
+      end;
+    {error, timeout} ->
+      timeout;
+    {error, Reason} ->
+      {error, Reason}
+  end.
+
+%% @doc Decode string into a request.
+%%
+%%   Function dies (`badmatch' or `case_clause') when the request is
+%%   malformed.
+
+-spec decode_request(binary()) ->
+    request_call()
+  | request_cancel()
+  | request_get_result()
+  | request_follow_stream()
+  | request_read_stream()
+  | no_return().
+
+decode_request(Line) ->
+  % decoded object is supposed to be a JSON hash
+  {ok, [{_,_} | _] = Request} = korrpc_json:decode(Line),
+  {ok, 1} = orddict:find(<<"korrpcdid">>, Request),
+  Request1 = orddict:erase(<<"korrpcdid">>, Request),
+  case Request1 of
+    [{<<"arguments">>, Args}, {<<"host">>, Host},
+      {<<"procedure">>, Procedure}] ->
+      {call, Procedure, Args, Host, undefined, undefined};
+    [{<<"arguments">>, Args}, {<<"host">>, Host},
+      {<<"procedure">>, Procedure}, {<<"timeout">>, Timeout}] ->
+      {call, Procedure, Args, Host, Timeout, undefined};
+    [{<<"arguments">>, Args}, {<<"host">>, Host},
+      {<<"max_exec_time">>, MaxExecTime}, {<<"procedure">>, Procedure}] ->
+      {call, Procedure, Args, Host, undefined, MaxExecTime};
+    [{<<"arguments">>, Args}, {<<"host">>, Host},
+      {<<"max_exec_time">>, MaxExecTime}, {<<"procedure">>, Procedure},
+      {<<"timeout">>, Timeout}] ->
+      {call, Procedure, Args, Host, Timeout, MaxExecTime};
+
+    [{<<"cancel">>, JobID}] ->
+      {cancel, binary_to_list(JobID)};
+
+    [{<<"get_result">>, JobID}] ->
+      {get_result, binary_to_list(JobID), no_wait};
+    [{<<"get_result">>, JobID}, {<<"wait">>, false}] ->
+      {get_result, binary_to_list(JobID), no_wait};
+    [{<<"get_result">>, JobID}, {<<"wait">>, true}] ->
+      {get_result, binary_to_list(JobID), wait};
+
+    [{<<"follow_stream">>, JobID}] ->
+      % XXX: `recent 0', as specified in the protocol; different from
+      % `read_stream'
+      {follow_stream, binary_to_list(JobID), recent, 0};
+    [{<<"follow_stream">>, JobID}, {<<"recent">>, RecentCount}] ->
+      {follow_stream, binary_to_list(JobID), recent, RecentCount};
+    [{<<"follow_stream">>, JobID}, {<<"since">>, Since}] ->
+      {follow_stream, binary_to_list(JobID), since, Since};
+
+    [{<<"read_stream">>, JobID}] ->
+      % XXX: `since 0', as specified in the protocol; different from
+      % `follow_stream'
+      {read_stream, binary_to_list(JobID), since, 0};
+    [{<<"read_stream">>, JobID}, {<<"recent">>, RecentCount}] ->
+      {read_stream, binary_to_list(JobID), recent, RecentCount};
+    [{<<"read_stream">>, JobID}, {<<"since">>, Since}] ->
+      {read_stream, binary_to_list(JobID), since, Since}
+  end.
 
 %%%---------------------------------------------------------------------------
 %%% vim:ft=erlang:foldmethod=marker
