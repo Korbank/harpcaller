785555f51a6f7f0c47c78b42f877b2c02f436172
Stanislaw Klekot <dozzie@jarowit.net>
2016-01-04 18:40:51 +0100

    Pushed a lot of queueing code around, making the korrpcdid_call_queue flatter.
    This should make intentions in the module clearer.

--- dispatcher/src/korrpcdid_call_queue.erl
+++ dispatcher/src/korrpcdid_call_queue.erl
@@ -55,6 +55,10 @@
 %%%---------------------------------------------------------------------------
 
 %% @doc Request a place in a named queue.
+%%
+%%   Function returns a queue reference `QRef'. When the time comes (the
+%%   requested queue has less than `Concurrency' running processes), the
+%%   calling process will get `{go, QRef}' message.
 
 -spec enqueue(queue_name(), pos_integer()) ->
   reference().
@@ -117,31 +121,19 @@ terminate(_Arg, _State = #state{q = Queue, qopts = QueueOpts,
 %% @private
 %% @doc Handle {@link gen_server:call/2}.
 
-handle_call({enqueue, Pid, QueueName, Concurrency} = _Request, _From,
-            State = #state{q = Queue, qopts = QueueOpts, qpids = QueuePids}) ->
-  MonRef = monitor(process, Pid),
+handle_call({enqueue, Pid, QueueName, Concurrency} = _Request, _From, State) ->
   QRef = make_ref(),
+  MonRef = remember_process(Pid, QRef, QueueName, State),
   Entry = #qentry{pid = Pid, monref = MonRef, qref = QRef},
-  PidEntry = #qpid{key = {Pid, MonRef}, q = QueueName, qref = QRef},
   % create a queue if it doesn't exist already
-  ets:insert_new(QueueOpts, #qopts{
-    name = QueueName,
-    running = 0,
-    concurrency = Concurrency
-  }),
-  case ets:lookup(QueueOpts, QueueName) of
-    [#qopts{running = N, concurrency = C}] when N < C ->
+  create_queue(QueueName, Concurrency, State),
+  case queue_status(QueueName, State) of
+    #qopts{running = N, concurrency = C} when N < C ->
       % queue with spare room to run a task
-      ets:update_counter(QueueOpts, QueueName, {#qopts.running, 1}),
-      ets:insert(QueuePids, PidEntry#qpid{state = running}),
-      ets:insert(Queue, {{running, QueueName}, Entry}),
-      make_running(Entry),
-      ok;
-    [#qopts{running = N, concurrency = C}] when N >= C ->
+      make_running(Entry, QueueName, State);
+    #qopts{running = N, concurrency = C} when N >= C ->
       % queue full of running tasks
-      ets:insert(QueuePids, PidEntry#qpid{state = queued}),
-      ets:insert(Queue, {{queued, QueueName}, Entry}),
-      ok
+      add_queued(Entry, QueueName, State)
   end,
   {reply, QRef, State};
 
@@ -159,38 +151,33 @@ handle_cast(_Request, State) ->
 %% @private
 %% @doc Handle incoming messages.
 
-handle_info({'DOWN', MonRef, process, Pid, _Info} = _Message,
-            State = #state{q = Queue, qopts = QueueOpts, qpids = QueuePids}) ->
-  case ets:lookup(QueuePids, {Pid, MonRef}) of
-    [] ->
+handle_info({'DOWN', MonRef, process, Pid, _Info} = _Message, State) ->
+  case recall_process(Pid, MonRef, State) of
+    none ->
       % it wasn't a process monitored because of queue
       ignore;
-    [#qpid{q = QueueName, qref = QRef, state = running}] ->
+    {QRef, QueueName, running} ->
       Entry = #qentry{pid = Pid, monref = MonRef, qref = QRef},
-      ets:delete_object(Queue, {{running, QueueName}, Entry}),
-      case ets:lookup(Queue, {queued, QueueName}) of
-        [] ->
-          % if nothing is left for this queue, check how many processes are
-          % running there, and if none (this was the last one), drop the queue
-          case ets:update_counter(QueueOpts, QueueName, {#qopts.running, -1}) of
-            N when N > 0 -> ok;
-            0 -> ets:delete(QueueOpts, QueueName)
-          end;
-        [{{queued, QueueName}, NextEntry} | _] ->
-          ets:delete_object(Queue, {{queued, QueueName}, NextEntry}),
-          ets:insert(Queue, {{running, QueueName}, NextEntry}),
-          NextEntryKey = {NextEntry#qentry.pid, NextEntry#qentry.monref},
-          ets:update_element(QueuePids, NextEntryKey, {#qpid.state, running}),
-          make_running(NextEntry)
+      RunningCount = delete_running(Entry, QueueName, State),
+      QueuedTasks = list_queued(QueueName, State),
+      case {QueuedTasks, RunningCount} of
+        {[], _} when RunningCount > 0 ->
+          % nothing left in queue, but there's still some processes running
+          ok;
+        {[], 0} ->
+          % nothing left in queue and this was the last running process
+          delete_queue(QueueName, State);
+        {[NextEntry | _], _} ->
+          % still something in the queue; make it running
+          make_running(NextEntry, QueueName, State)
       end;
-    [#qpid{q = QueueName, qref = QRef, state = queued}] ->
+    {QRef, QueueName, queued} ->
       % NOTE: if this one is queued, it means the queue was full, so don't
       % delete it (the queue) just yet
       Entry = #qentry{pid = Pid, monref = MonRef, qref = QRef},
-      ets:delete_object(Queue, {{queued, QueueName}, Entry}),
+      delete_queued(Entry, QueueName, State),
       ok
   end,
-  ets:delete(QueuePids, {Pid, MonRef}),
   {noreply, State};
 
 %% unknown messages
@@ -211,10 +198,180 @@ code_change(_OldVsn, State, _Extra) ->
 %%----------------------------------------------------------
 
 %%%---------------------------------------------------------------------------
+%%% helpers for working with whole queues {{{
 
-make_running(_Entry = #qentry{pid = Pid, qref = QRef}) ->
+%% @doc Create a queue with specified concurrency level.
+%%   This mainly records options for the queue with running counter starting
+%%   at 0, as the queue's content is {@type #qentry@{@}} under (typically
+%%   duplicated) key of queue name.
+
+-spec create_queue(queue_name(), pos_integer(), #state{}) ->
+  true | false.
+
+create_queue(QueueName, Concurrency, _State = #state{qopts = QueueOpts}) ->
+  ets:insert_new(QueueOpts, #qopts{
+    name = QueueName,
+    running = 0,
+    concurrency = Concurrency
+  }).
+
+%% @doc Get status of the queue.
+%%   Returns queue's options, which include recorded running counter.
+
+-spec queue_status(queue_name(), #state{}) ->
+  #qopts{}.
+
+queue_status(QueueName, _State = #state{qopts = QueueOpts}) ->
+  [Q] = ets:lookup(QueueOpts, QueueName),
+  Q.
+
+%% @doc Delete remains of the queue.
+%%   Namely, the queue's options.
+
+-spec delete_queue(queue_name(), #state{}) ->
+  true.
+
+delete_queue(QueueName, _State = #state{qopts = QueueOpts}) ->
+  ets:delete(QueueOpts, QueueName).
+
+%%% }}}
+%%%---------------------------------------------------------------------------
+%%% helpers for working with enqueued tasks {{{
+
+%% @doc Add to a queue a task that will be waiting for execution.
+%%
+%%   Function <em>does not</em> update running counter in {@link
+%%   queue_status/2. queue status} (because why should it?).
+
+-spec add_queued(#qentry{}, queue_name(), #state{}) ->
+  ok.
+
+add_queued(Entry, QueueName, _State = #state{q = Queue}) ->
+  ets:insert(Queue, {{queued, QueueName}, Entry}),
+  ok.
+
+%% @doc Delete from a queue a task that is still waiting for execution.
+%%   The task has just terminated.
+
+-spec delete_queued(#qentry{}, queue_name(), #state{}) ->
+  ok.
+
+delete_queued(Entry, QueueName, _State = #state{q = Queue}) ->
+  ets:delete_object(Queue, {{queued, QueueName}, Entry}),
+  ok.
+
+%% @doc Delete from a queue a task that was running.
+%%   The task has just terminated.
+%%
+%%   Function updates running counter in {@link queue_status/2. queue status}
+%%   and returns its new value.
+
+-spec delete_running(#qentry{}, queue_name(), #state{}) ->
+  non_neg_integer().
+
+delete_running(Entry, QueueName,
+               _State = #state{q = Queue, qopts = QueueOpts}) ->
+  ets:delete_object(Queue, {{running, QueueName}, Entry}),
+  _Count = ets:update_counter(QueueOpts, QueueName, {#qopts.running, -1}).
+
+%% @doc List tasks waiting for execution in specified queue.
+%%   Tasks are returned in order of their appearance, as {@link ets}
+%%   guarantees for tables of type `bag' (see {@link ets:lookup/2}).
+
+-spec list_queued(queue_name(), #state{}) ->
+  [#qentry{}].
+
+list_queued(QueueName, _State = #state{q = Queue}) ->
+  % strip the key from result tuples
+  [E || {{queued, _}, E} <- ets:lookup(Queue, {queued, QueueName})].
+
+%% @doc Mark the task as running and tell it to start.
+%%
+%%   The task might or might not be added earlier as waiting for execution,
+%%   both cases are handled.
+%%
+%%   The task (process) is sent a message `{go, Ref}', where `Ref' is the
+%%   reference returned by {@link enqueue/2}.
+%%
+%%   Function updates running counter in {@link queue_status/2. queue status}.
+
+-spec make_running(#qentry{}, queue_name(), #state{}) ->
+  ok.
+
+make_running(Entry = #qentry{pid = Pid, monref = MonRef, qref = QRef},
+             QueueName, State = #state{q = Queue, qopts = QueueOpts}) ->
+  delete_queued(Entry, QueueName, State), % if there was any
+  update_process_state(Pid, MonRef, running, State),
+  ets:update_counter(QueueOpts, QueueName, {#qopts.running, 1}),
+  ets:insert(Queue, {{running, QueueName}, Entry}),
   Pid ! {go, QRef},
   ok.
 
+%%% }}}
+%%%---------------------------------------------------------------------------
+%%% helpers for remembering/monitoring enqueued tasks {{{
+
+%% @doc Monitor task, remembering some details about it.
+%%   Function returns reference as returned by `monitor/2' (i.e., expect
+%%   a message ``{'DOWN',...}'' with this reference).
+%%
+%%   `QRef' is a queue reference that will be returned to the enqueued
+%%   process (see {@link enqueue/2}).
+%%
+%%   The process will be remembered with running state as `queued'. Use
+%%   {@link update_process_state/4} to change that.
+%%
+%%   This function must be called prior to a {@link make_running/3} call.
+%%   Simply calling it as a first thing on every arriving enqueueing request
+%%   should be enough.
+
+-spec remember_process(pid(), reference(), queue_name(), #state{}) ->
+  reference().
+
+remember_process(Pid, QRef, QueueName,
+                 _State = #state{qpids = QueuePids}) ->
+  MonRef = monitor(process, Pid),
+  ets:insert(QueuePids, #qpid{
+    key = {Pid, MonRef},
+    q = QueueName,
+    qref = QRef,
+    state = queued % will be updated by `make_running()'
+  }),
+  MonRef.
+
+%% @doc Update remembered running state about monitored process.
+%%   This function is mainly used when a process starts its execution.
+
+-spec update_process_state(pid(), reference(), running | queued, #state{}) ->
+  ok.
+
+update_process_state(Pid, MonRef, NewProcState,
+                     _State = #state{qpids = QueuePids}) ->
+  ets:update_element(QueuePids, {Pid, MonRef}, {#qpid.state, NewProcState}),
+  ok.
+
+%% @doc Recall the task details about a (monitored) process that has
+%%   terminated. The details include queue reference returned to the process,
+%%   name of its queue and its running state (either `running' or `queued').
+%%
+%%   If the process was not monitored using {@link remember_process/4}, `none'
+%%   is returned.
+
+-spec recall_process(pid(), reference(), #state{}) ->
+  {QRef :: reference(), queue_name(), running | queued} | none.
+
+recall_process(Pid, MonRef, _State = #state{qpids = QueuePids}) ->
+  case ets:lookup(QueuePids, {Pid, MonRef}) of
+    [] ->
+      none;
+    [#qpid{qref = QRef, q = QueueName, state = running}] ->
+      ets:delete(QueuePids, {Pid, MonRef}), % it will no longer be necessary
+      {QRef, QueueName, running};
+    [#qpid{qref = QRef, q = QueueName, state = queued}] ->
+      ets:delete(QueuePids, {Pid, MonRef}), % it will no longer be necessary
+      {QRef, QueueName, queued}
+  end.
+
+%%% }}}
 %%%---------------------------------------------------------------------------
 %%% vim:ft=erlang:foldmethod=marker
