5424842bbfaba0f6a611c89aa7ef48b18b96e9d7
Stanislaw Klekot <dozzie@jarowit.net>
2016-01-04 16:52:57 +0100

    Implemented call requests queue.
    Now it needs to be used by korrpcdid_caller.

--- dispatcher/src/korrpcdid_call_queue.erl
+++ dispatcher/src/korrpcdid_call_queue.erl
@@ -8,6 +8,9 @@
 
 -behaviour(gen_server).
 
+%% public interface
+-export([enqueue/2]).
+
 %% supervision tree API
 -export([start/0, start_link/0]).
 
@@ -19,9 +22,46 @@
 %%%---------------------------------------------------------------------------
 %%% type specification/documentation {{{
 
--record(state, {}).
+-type queue_name() :: term().
+
+-record(state, {
+  q :: ets:tab(),     % running and queued objects
+  qopts :: ets:tab(), % queue options (just concurrency level at the moment)
+  qpids :: ets:tab()  % pid->{queue+status}
+}).
+
+-record(qopts, {
+  name :: queue_name(),
+  running :: non_neg_integer(), % when 0, queue is deleted
+  concurrency :: pos_integer()  % running =< concurrency
+}).
+
+-record(qentry, {
+  pid :: pid(),
+  monref :: reference(),
+  qref :: reference()
+}).
+
+-record(qpid, {
+  key :: {pid(), reference()},
+  q :: queue_name(),
+  qref :: reference(),
+  state :: queued | running
+}).
 
 %%% }}}
+%%%---------------------------------------------------------------------------
+%%% public interface
+%%%---------------------------------------------------------------------------
+
+%% @doc Request a place in a named queue.
+
+-spec enqueue(queue_name(), pos_integer()) ->
+  reference().
+
+enqueue(QueueName, Concurrency) when is_integer(Concurrency), Concurrency > 0 ->
+  gen_server:call(?MODULE, {enqueue, self(), QueueName, Concurrency}).
+
 %%%---------------------------------------------------------------------------
 %%% supervision tree API
 %%%---------------------------------------------------------------------------
@@ -49,13 +89,25 @@ start_link() ->
 %% @doc Initialize event handler.
 
 init(_Args) ->
-  State = #state{},
+  Queue = ets:new('queue', [bag]),
+  QueueOpts = ets:new(queue_opts, [set, {keypos, #qopts.name}]),
+  QueuePids = ets:new(queue_pids, [set, {keypos, #qpid.key}]),
+  State = #state{
+    q = Queue,
+    qopts = QueueOpts,
+    qpids = QueuePids
+  },
   {ok, State}.
 
 %% @private
 %% @doc Clean up after event handler.
 
-terminate(_Arg, _State) ->
+terminate(_Arg, _State = #state{q = Queue, qopts = QueueOpts,
+                                qpids = QueuePids}) ->
+  % TODO: what if there's still stuff running and/or queued?
+  ets:delete(Queue),
+  ets:delete(QueueOpts),
+  ets:delete(QueuePids),
   ok.
 
 %% }}}
@@ -64,6 +116,34 @@ terminate(_Arg, _State) ->
 
 %% @private
 %% @doc Handle {@link gen_server:call/2}.
+
+handle_call({enqueue, Pid, QueueName, Concurrency} = _Request, _From,
+            State = #state{q = Queue, qopts = QueueOpts, qpids = QueuePids}) ->
+  MonRef = monitor(process, Pid),
+  QRef = make_ref(),
+  Entry = #qentry{pid = Pid, monref = MonRef, qref = QRef},
+  PidEntry = #qpid{key = {Pid, MonRef}, q = QueueName, qref = QRef},
+  % create a queue if it doesn't exist already
+  ets:insert_new(QueueOpts, #qopts{
+    name = QueueName,
+    running = 0,
+    concurrency = Concurrency
+  }),
+  case ets:lookup(QueueOpts, QueueName) of
+    [#qopts{running = N, concurrency = C}] when N < C ->
+      % queue with spare room to run a task
+      ets:update_counter(QueueOpts, QueueName, {#qopts.running, 1}),
+      ets:insert(QueuePids, PidEntry#qpid{state = running}),
+      ets:insert(Queue, {{running, QueueName}, Entry}),
+      make_running(Entry),
+      ok;
+    [#qopts{running = N, concurrency = C}] when N >= C ->
+      % queue full of running tasks
+      ets:insert(QueuePids, PidEntry#qpid{state = queued}),
+      ets:insert(Queue, {{queued, QueueName}, Entry}),
+      ok
+  end,
+  {reply, QRef, State};
 
 %% unknown calls
 handle_call(_Request, _From, State) ->
@@ -78,6 +158,40 @@ handle_cast(_Request, State) ->
 
 %% @private
 %% @doc Handle incoming messages.
+
+handle_info({'DOWN', MonRef, process, Pid, _Info} = _Message,
+            State = #state{q = Queue, qopts = QueueOpts, qpids = QueuePids}) ->
+  case ets:lookup(QueuePids, {Pid, MonRef}) of
+    [] ->
+      % it wasn't a process monitored because of queue
+      ignore;
+    [#qpid{q = QueueName, qref = QRef, state = running}] ->
+      Entry = #qentry{pid = Pid, monref = MonRef, qref = QRef},
+      ets:delete_object(Queue, {{running, QueueName}, Entry}),
+      case ets:lookup(Queue, {queued, QueueName}) of
+        [] ->
+          % if nothing is left for this queue, check how many processes are
+          % running there, and if none (this was the last one), drop the queue
+          case ets:update_counter(QueueOpts, QueueName, {#qopts.running, -1}) of
+            N when N > 0 -> ok;
+            0 -> ets:delete(QueueOpts, QueueName)
+          end;
+        [{{queued, QueueName}, NextEntry} | _] ->
+          ets:delete_object(Queue, {{queued, QueueName}, NextEntry}),
+          ets:insert(Queue, {{running, QueueName}, NextEntry}),
+          NextEntryKey = {NextEntry#qentry.pid, NextEntry#qentry.monref},
+          ets:update_element(QueuePids, NextEntryKey, {#qpid.state, running}),
+          make_running(NextEntry)
+      end;
+    [#qpid{q = QueueName, qref = QRef, state = queued}] ->
+      % NOTE: if this one is queued, it means the queue was full, so don't
+      % delete it (the queue) just yet
+      Entry = #qentry{pid = Pid, monref = MonRef, qref = QRef},
+      ets:delete_object(Queue, {{queued, QueueName}, Entry}),
+      ok
+  end,
+  ets:delete(QueuePids, {Pid, MonRef}),
+  {noreply, State};
 
 %% unknown messages
 handle_info(_Message, State) ->
@@ -95,6 +209,12 @@ code_change(_OldVsn, State, _Extra) ->
 
 %% }}}
 %%----------------------------------------------------------
+
+%%%---------------------------------------------------------------------------
+
+make_running(_Entry = #qentry{pid = Pid, qref = QRef}) ->
+  Pid ! {go, QRef},
+  ok.
 
 %%%---------------------------------------------------------------------------
 %%% vim:ft=erlang:foldmethod=marker
