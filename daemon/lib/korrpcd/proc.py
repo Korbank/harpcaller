fda8e417345473feccea981a18448070634f8607
Stanislaw Klekot <dozzie@jarowit.net>
2016-02-03 12:30:05 +0100

    Added a way of reporting a return value from streaming procedure.

--- daemon/lib/korrpcd/proc.py
+++ daemon/lib/korrpcd/proc.py
@@ -3,9 +3,16 @@
 Interface for published procedures
 ----------------------------------
 
+In simple cases, you may use decorators (:func:`procedure()` and
+:func:`streaming_procedure()`) to mark function as a procedure intended for
+remote calls. In more sophisticated cases, you may create a subclass of
+:class:`Procedure` or :class:`StreamingProcedure`.
+
 .. decorator:: procedure
 
    Mark the function as a remote callable procedure.
+
+   Whatever the function returns, it will be sent as a result.
 
    See :class:`Procedure`.
 
@@ -14,13 +21,38 @@ Interface for published procedures
    Mark the function as a remote callable procedure that returns streamed
    result.
 
-   See :class:`StreamingProcedure`.
+   Function produces the stream by using ``yield msg`` (i.e., by returning an
+   iterator). To return a value, function should yield :class:`Result`
+   object, which will be the last object consumed. If function does not yield
+   :class:`Result`, reported returned value will be simply ``None``.
+
+   See :class:`StreamingProcedure`, :class:`Result`.
+
+Examples of using decorators::
+
+    @procedure
+    def sum_and_difference(a, b):
+        return {"sum": a + b, "difference": a - b}
+
+    @stream_result
+    def stream():
+        for i in xrange(1, 10):
+            yield {"i": i}
+            time.sleep(1)
+        yield Result({"msg": "end of xrange"})
+
+.. autoclass:: Result
+   :members:
 
 .. autoclass:: Procedure
    :members:
 
+   .. automethod:: __call__
+
 .. autoclass:: StreamingProcedure
    :members:
+
+   .. automethod:: __call__
 
 '''
 #-----------------------------------------------------------------------------
@@ -36,6 +68,11 @@ class Procedure(object):
         self.function = function
 
     def __call__(self, *args, **kwargs):
+        '''
+        :return: call result
+
+        Execute the procedure and return its result.
+        '''
         return self.function(*args, **kwargs)
 
 # XXX: promised decorator
@@ -52,18 +89,45 @@ class StreamingProcedure(Procedure):
     ``yield``.
     '''
 
+    def __init__(self, function):
+        super(StreamingProcedure, self).__init__(function)
+        self._result = None
+
+    def __call__(self, *args, **kwargs):
+        '''
+        :return: stream result
+        :rtype: iterator
+
+        Execute the procedure and return its streamed result as an iterator.
+        To report returned value, use :meth:`result()`.
+        '''
+        for msg in self.function(*args, **kwargs):
+            if isinstance(msg, Result):
+                self._result = msg.value
+                break
+            yield msg
+
     def result(self):
         '''
         :return: call result
 
         Return the result that the procedure was supposed to return.
         '''
-        # method to be replaced by a subclass, so the procedure can return
-        # stream and a value at the same time
-        return None
+        return self._result
 
 # XXX: promised decorator
 streaming_procedure = StreamingProcedure
+
+#-----------------------------------------------------------------------------
+
+class Result(object):
+    '''
+    :param value: value to be returned
+
+    Yield a message to be resulting value from :class:`StreamingProcedure`.
+    '''
+    def __init__(self, value):
+        self.value = value
 
 #-----------------------------------------------------------------------------
 # vim:ft=python:foldmethod=marker
