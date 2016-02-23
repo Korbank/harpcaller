a8ff76a93d2155b76af0384f5d36efae13f9f83d
Stanislaw Klekot <dozzie@jarowit.net>
2016-02-23 15:13:08 +0100

    Extended harpd.proc interface for setuid()/setgid()/timeout support.
    It still needs to be implemented in harpd.daemon to honour them.

--- daemon/lib/harpd/proc.py
+++ daemon/lib/harpd/proc.py
@@ -9,14 +9,19 @@ remote calls. In more sophisticated cases, you may create a subclass of
 :class:`Procedure` or :class:`StreamingProcedure`.
 
 .. decorator:: procedure
+.. decorator:: procedure(timeout = ..., uid = ..., gid = ...)
 
    Mark the function as a remote callable procedure.
 
    Whatever the function returns, it will be sent as a result.
 
+   The second form allows to set options, like UID/GID to run as (either
+   numeric or name) or time the function execution will take.
+
    See :class:`Procedure`.
 
 .. decorator:: streaming_procedure
+.. decorator:: streaming_procedure(timeout = ..., uid = ..., gid = ...)
 
    Mark the function as a remote callable procedure that returns streamed
    result.
@@ -26,16 +31,36 @@ remote calls. In more sophisticated cases, you may create a subclass of
    object, which will be the last object consumed. If function does not yield
    :class:`Result`, reported returned value will be simply ``None``.
 
+   The second form allows to set options, like UID/GID to run as (either
+   numeric or name) or time the function execution will take.
+
    See :class:`StreamingProcedure`, :class:`Result`.
 
 Examples of using decorators::
 
     from harpd.proc import procedure, streaming_procedure, Result
     import time
+    import subprocess
+    import re
+
+    _UPTIME_RE = re.compile(
+        r'^..:..:.. up (.*),'
+        r'  \d users?,'
+        r'  load average: (\d\.\d\d), (\d\.\d\d), (\d\.\d\d)$'
+    )
 
     @procedure
     def sum_and_difference(a, b):
         return {"sum": a + b, "difference": a - b}
+
+    @procedure(uid = "nobody")
+    def uptime():
+        uptime_output = subprocess.check_output("uptime").strip()
+        (uptime, load1, load5, load15) = _UPTIME_RE.match(uptime_output).groups()
+        return {
+            "uptime": uptime,
+            "load average": { "1": load1, "5": load5, "15": load15 },
+        }
 
     @streaming_procedure
     def stream():
@@ -52,10 +77,43 @@ Examples of using decorators::
 
    .. automethod:: __call__
 
+   .. attribute:: function
+
+      function to call
+
+   .. attribute:: timeout
+
+      time after which the process running the function will be terminated
+
+   .. attribute:: uid
+
+      UID or username to run the function as
+
+   .. attribute:: gid
+
+      GID or group name to run the function as
+
 .. autoclass:: StreamingProcedure
    :members:
+   :inherited-members:
 
    .. automethod:: __call__
+
+   .. attribute:: function
+
+      function to call
+
+   .. attribute:: timeout
+
+      time after which the process running the function will be terminated
+
+   .. attribute:: uid
+
+      UID or username to run the function as
+
+   .. attribute:: gid
+
+      GID or group name to run the function as
 
 '''
 #-----------------------------------------------------------------------------
@@ -64,11 +122,18 @@ class Procedure(object):
     '''
     :param function: function to wrap
     :type function: callable
+    :param timeout:
+    :param uid:
+    :param gid:
 
     Simple callable wrapper over a function.
     '''
-    def __init__(self, function):
+    def __init__(self, function, timeout = None, uid = None, gid = None):
         self.function = function
+        self.timeout = timeout
+        self.uid = uid
+        self.gid = gid
+        self.__doc__ = function.__doc__
 
     def __call__(self, *args, **kwargs):
         '''
@@ -78,8 +143,14 @@ class Procedure(object):
         '''
         return self.function(*args, **kwargs)
 
-# XXX: promised decorator
-procedure = Procedure
+def procedure(function = None, timeout = None, uid = None, gid = None):
+    def wrapper(function):
+        return Procedure(function, timeout = timeout, uid = uid, gid = gid)
+
+    if function is None:
+        return wrapper
+    else:
+        return wrapper(function)
 
 #-----------------------------------------------------------------------------
 
@@ -92,8 +163,9 @@ class StreamingProcedure(Procedure):
     ``yield``.
     '''
 
-    def __init__(self, function):
-        super(StreamingProcedure, self).__init__(function)
+    def __init__(self, function, timeout = None, uid = None, gid = None):
+        super(StreamingProcedure, self).__init__(function, timeout = None,
+                                                 uid = None, gid = None)
         self._result = None
 
     def __call__(self, *args, **kwargs):
@@ -118,8 +190,16 @@ class StreamingProcedure(Procedure):
         '''
         return self._result
 
-# XXX: promised decorator
-streaming_procedure = StreamingProcedure
+def streaming_procedure(function = None, timeout = None,
+                        uid = None, gid = None):
+    def wrapper(function):
+        return StreamingProcedure(function, timeout = timeout,
+                                  uid = uid, gid = gid)
+
+    if function is None:
+        return wrapper
+    else:
+        return wrapper(function)
 
 #-----------------------------------------------------------------------------
 
